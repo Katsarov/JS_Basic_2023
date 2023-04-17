2832494c3891c0f5918595097a43bd1df4018c7c
Kalin Katsarov <k.l.katsarov@gmail.com>
2023-04-17 19:43:01 +0300

    Make some changes

--- hello.js
+++ hello.js
@@ -1,11 +1,5 @@
-function radius(r) {
-    if (typeof (r) === 'number') {
-        let area = Math.PI * r ** 2;
-        console.log(Number(area.toFixed(2)));
-    } else {
-        console.log(`We can not calculate the circle area, because we receive a ${typeof (r)}.`);
-    }
+function hello() {
+    console.log('Hello, world!');
 }
 
-radius(5);
-radius('boko');
+hello();
